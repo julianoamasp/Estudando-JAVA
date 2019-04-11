40e45ac8ede4609bdec3fcdf6f1219d75788e19c
juliano <julianoamasp@gmail.com>
2019-04-10 23:47:36 -0300

    mudoulo 6

---
+++ src/moludo6/ClasseMath.java
@@ -0,0 +1,19 @@
+package moludo6;
+
+/**
+ *
+ * @author JULIANO
+ */
+public class ClasseMath {
+    public static void main(String args[]){
+        
+        //Retorna um valor absoluto
+        System.out.println(Math.abs(4)); 
+        
+        //arredonda para o emnor interior
+        System.out.println(Math.ceil(20.2342353)); 
+        
+        
+        
+    }
+}
